04aa0f589a95bd1a7f697f68e397733134e52c15
Matías <crosserain@gmail.com>
2023-10-24 18:33:29 -0300

    message controller create message

---
+++ controller/messageController.js
@@ -0,0 +1,13 @@
+//initial config
+const express = require("express");
+const messageDB = require("../model/messageModel");
+const app = express();
+
+app.post("/api/email/send", createMessage);
+
+function createMessage(req, res) {
+    messageDB.create(req.body, (err, result) => {
+        if (err) return res.status(500).send(err.code);
+        res.send("email sent");
+    });
+}
