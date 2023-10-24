2816789c784e37ec805eac8722d2011ffe28b12e
Matías <crosserain@gmail.com>
2023-10-23 22:15:21 -0300

    users controller: create user

---
+++ controller/usersController.js
@@ -0,0 +1,13 @@
+const usersDB = require("../model/usersModel");
+const express = require("express");
+const app = express();
+
+app.post("/api/email/user", createUser);
+
+function createUser(req, res) {
+    const userData = req.body;
+    usersDB.create(userData, (err, result) => {
+        if(err) return res.status(500).send(err);
+        res.send(result);
+    });
+}
