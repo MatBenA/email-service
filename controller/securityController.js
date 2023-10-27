46158cc53c5fe2d9da568d1983b1ac8def3bcada
Matías <crosserain@gmail.com>
2023-10-26 22:01:33 -0300

    added: security token sent when user logs in

--- controller/securityController.js
+++ controller/securityController.js
@@ -3,15 +3,21 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const usersDB = require("../model/usersModel");
+require("dotenv").config();
 
 app.post("/api/email/login", login);
 
 async function login(req, res) {
     usersDB.loginData(req.body, (err, result, match) => {
         if (err) return res.status(500).send(err);
-        if (result === "404" || !match)return res.status(404).send("Invalid user or password");
-        
-        const token = jwt.sign()
+        if (result === "404" || !match)
+            return res.status(404).send("Invalid user or password");
+
+        const user = JSON.stringify(result[0]);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "1d",
+        });
+        res.json({ token });
     });
 }
 
