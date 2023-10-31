4a350d53b7480b37f57d70c4172409000532d120
Matías <crosserain@gmail.com>
2023-10-31 00:45:48 -0300

    new database design

--- email-serviceDB.sql
+++ email-serviceDB.sql
@@ -9,38 +9,39 @@ CREATE TABLE users (
 	last_name VARCHAR(50) NOT NULL,
 	password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP,
     PRIMARY KEY (user_id)
-);
-
-CREATE TABLE status (
-	status_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (40),
-    PRIMARY KEY (status_id)
 );
 
 CREATE TABLE messages (
 	message_id INT NOT NULL AUTO_INCREMENT,
     subject VARCHAR(255),
     message VARCHAR(10000),
-    is_read BOOL DEFAULT FALSE,
     user_id INT NOT NULL,
-    status_id INT DEFAULT 2,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP,
     PRIMARY KEY (message_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (status_id) REFERENCES status(status_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE refresh_tokens (
-	token VARCHAR(500)
+	user_id INT NOT NULL,
+	token VARCHAR(500),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO status (name) VALUES ("inbox");
-INSERT INTO status (name) VALUES ("sent");
-INSERT INTO status (name) VALUES ("draft");
-INSERT INTO status (name) VALUES ("spam");
-INSERT INTO status (name) VALUES ("trash");
+CREATE TABLE hosters (
+	hoster_id INT NOT NULL AUTO_INCREMENT,
+    host VARCHAR(500),
+    PRIMARY KEY (hoster_id)
+);
+
+CREATE TABLE connections (
+    port INT NOT NULL DEFAULT 587,
+    password VARCHAR(500) NOT NULL,
+    hoster_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (hoster_id)
+        REFERENCES hosters (hoster_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+);
+    
