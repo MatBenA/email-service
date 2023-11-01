87e8acb6429389770277a3bd4a017d1d7d3a9ec8
Matías <crosserain@gmail.com>
2023-11-01 16:45:23 -0300

    mailgun and sendgrid templates

---
+++ emailabstraction/mailgun.mjs
@@ -0,0 +1,23 @@
+const API_KEY = 'YOUR_API_KEY';
+const DOMAIN = 'YOUR_DOMAIN_NAME';
+
+ import formData from 'form-data';
+ import Mailgun from 'mailgun.js';
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: 'api', key: API_KEY});
+
+const messageData = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'foo@example.com, bar@example.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomeness!'
+};
+
+client.messages.create(DOMAIN, messageData)
+ .then((res) => {
+   console.log(res);
+ })
+ .catch((err) => {
+   console.error(err);
+ });
